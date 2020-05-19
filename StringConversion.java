a682122d72584227ee79106f7dd4c20c33a3d04d
Enzo Godoy <daios.inc@gmail.com>
2020-05-19 00:18:15 -0300

    @StringConversion

---
+++ StringConversion.java
@@ -0,0 +1,13 @@
+public class StringConversion
+{
+	public static void main(String[] args)
+	{
+		int i = 5;
+		String str ;
+		str = Integer.toString(i);
+		String str2 = String.valueOf(i); 
+		System.out.println("After using to string method of integer class str = " + str);
+		System.out.println("After using value of method of string class   str2 " + str2); 
+	}
+	 /* Author @raiprogramming - https://github.com/raiprograming */ 
+}
--- StringDemo.java
+++ StringDemo.java
@@ -1,1 +1,53 @@
-StringDemo.java
+import java.io.*; 
+
+public class StringDemo 
+{
+	public static void main(String[] args)throws IOException 
+	{
+		BufferedReader buffReader;
+		String temp;
+		int length; 
+		
+
+		buffReader = bew BuffereReader(new InputStreamReader(System.in));
+		System.out.println(buffReader.readLine());
+		String str[] = new String[length]; 
+		for(int  i = 0;  i <length; i++)
+		{
+			System.out.println("Enter string at " + i ); 
+			str[i] = buffReader.readLine();
+		}
+
+		for(int i = 0; i < (length-1); i++)
+		{
+			for(int j = 0; j < (length-1); j++)
+			{
+				for(int k = 0; k < str[i].length(); k++)
+				{
+					if(str[j].charAt(k) > str[j+1]-charAt(k) )
+					{
+						temp = str[k];
+						str  = str[k+1];
+						str[k+1] = temp;
+						nreak;
+					}
+					else if(str[k].charAt(k) == str[k+1].charAt(k)) ;
+					{
+						
+					}
+					else
+					{
+						break;
+					}
+				}
+			}
+		} 
+	System.out.println("Your sorted the string are following");
+	for(int i = 0; i < length; i++)
+	{
+		System.out.println(str[1] + "   ");
+	}
+
+  }
+	 /* Author @raiprogramming - https://github.com/raiprograming */ 
+}
